b248c942d766a6c3c344a9ce6717866cf1e1d1f7
Roberto  di Rienzo <roberto.dirienzo@terradue.com>
2020-04-06 18:48:20 +0200

    EOEPCA-47 #comment tests added

--- travis/build.sh
+++ travis/build.sh
@@ -25,6 +25,6 @@ else
 
     source travis/libs/build.sh
 
-    docker run --rm -ti  -v $PWD:/project/ -w /project/build/  ${LOCAL_DOCKERIMAGE} ./tests/libtest-test
+    docker run --rm -ti  -v $PWD:/project/ -w /project/build/  ${LOCAL_DOCKERIMAGE} ./tests/libtest-test --gtest_break_on_failure
 
 fi
