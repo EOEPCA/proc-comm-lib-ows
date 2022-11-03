a041efe630f4eecdafc67800b67865a92f14e729
bbrauzzi <blasco.brauzzi@terradue.com>
2022-11-03 15:58:24 +0100

    new test case

--- scripts/build.sh
+++ scripts/build.sh
@@ -20,4 +20,4 @@ docker run --rm -ti -v $PWD:/project/ -w /project/build ${BUILDER_ENV_IMAGE} mak
 mkdir -p demo
 cp $PWD/tests/application/assets/ows2.xml $PWD/build/libeoepcaows.so $PWD/build/runner $PWD/build/tests/libtest-test demo
 
-cd demo && ./runner ows2.xml && ./libtest-test --gtest_break_on_failure
+cd demo && ./runner ows2.xml && ./libtest-test --gtest_break_on_failure  --gtest_filter=eoepcaows_02.inputs_enum_optional
--- tests/application/test02_run.cpp
+++ tests/application/test02_run.cpp
@@ -1,10 +1,8 @@
 #include "global.hpp"
 #include "gtest/gtest.h"
-
 #include "httpfuntions.hpp"
 
-
-void  printResults(const std::unique_ptr<EOEPCA::OWS::Param>& input){
+void printResults(const std::unique_ptr<EOEPCA::OWS::Param>& input) {
   std::cout << "--------------------------\n";
   std::cout << "getIdentifier: " << input->getIdentifier() << "\n";
   std::cout << "getType: " << input->getType() << "\n";
@@ -15,16 +13,13 @@ void  printResults(const std::unique_ptr<EOEPCA::OWS::Param>& input){
   std::cout << "getMinOccurs: " << input->getMinOccurs() << "\n";
   std::cout << "getMaxOccurs: " << input->getMaxOccurs() << "\n";
 
-
   if (input->getType() == "BoundingBoxData") {
-    auto data =
-        dynamic_cast<EOEPCA::OWS::BoundingBoxData*>(input.get());
+    auto data = dynamic_cast<EOEPCA::OWS::BoundingBoxData*>(input.get());
 
   } else if (input->getType() == "ComplexData") {
     auto data = dynamic_cast<EOEPCA::OWS::ComplexData*>(input.get());
 
-    std::cout << "getMaximumMegabytes: "
-              << data->getMaximumMegabytes() << "\n";
+    std::cout << "getMaximumMegabytes: " << data->getMaximumMegabytes() << "\n";
 
     std::cout << "getSupported: \n";
     for (auto& supp : data->getSupported()) {
@@ -44,58 +39,55 @@ void  printResults(const std::unique_ptr<EOEPCA::OWS::Param>& input){
   if (input->getType() == "LiteralData") {
     auto data = dynamic_cast<EOEPCA::OWS::LiteralData*>(input.get());
 
-    if ("int_parameter"==data->getIdentifier()){
-      EXPECT_EQ(data->getTitle(),"Title of int_parameter");
-      EXPECT_EQ(data->getAbstract(),"Abstract describing int_parameter");
-      EXPECT_EQ(data->getDataType(),"integer");
-      EXPECT_EQ(data->getMinOccurs(),1);
-      EXPECT_EQ(data->getMaxOccurs(),1);
+    if ("int_parameter" == data->getIdentifier()) {
+      EXPECT_EQ(data->getTitle(), "Title of int_parameter");
+      EXPECT_EQ(data->getAbstract(), "Abstract describing int_parameter");
+      EXPECT_EQ(data->getDataType(), "integer");
+      EXPECT_EQ(data->getMinOccurs(), 1);
+      EXPECT_EQ(data->getMaxOccurs(), 1);
     }
 
-    if ("string_parameter"==data->getIdentifier()){
-      EXPECT_EQ(data->getTitle(),"Title of string_parameter");
-      EXPECT_EQ(data->getAbstract(),"Abstract describing string_parameter");
-      EXPECT_EQ(data->getDataType(),"string");
-      EXPECT_EQ(data->getMinOccurs(),1);
-      EXPECT_EQ(data->getMaxOccurs(),1);
-      EXPECT_EQ(data->getDefaultValue(),"CIAO a te");
+    if ("string_parameter" == data->getIdentifier()) {
+      EXPECT_EQ(data->getTitle(), "Title of string_parameter");
+      EXPECT_EQ(data->getAbstract(), "Abstract describing string_parameter");
+      EXPECT_EQ(data->getDataType(), "string");
+      EXPECT_EQ(data->getMinOccurs(), 1);
+      EXPECT_EQ(data->getMaxOccurs(), 1);
+      EXPECT_EQ(data->getDefaultValue(), "default string");
     }
 
-    if ("string_parameter_arr"==data->getIdentifier()){
-      EXPECT_EQ(data->getTitle(),"Title of string_parameter_arr");
-      EXPECT_EQ(data->getAbstract(),"Abstract describing string_parameter_arr");
-      EXPECT_EQ(data->getDataType(),"string");
-      EXPECT_EQ(data->getMinOccurs(),1);
-      EXPECT_EQ(data->getMaxOccurs(),999);
+    if ("string_parameter_arr" == data->getIdentifier()) {
+      EXPECT_EQ(data->getTitle(), "Title of string_parameter_arr");
+      EXPECT_EQ(data->getAbstract(),
+                "Abstract describing string_parameter_arr");
+      EXPECT_EQ(data->getDataType(), "string");
+      EXPECT_EQ(data->getMinOccurs(), 1);
+      EXPECT_EQ(data->getMaxOccurs(), 999);
     }
 
-    if ("string_with_options_parameter"==data->getIdentifier()){
-      EXPECT_EQ(data->getTitle(),"Title of string_with_options_parameter");
-      EXPECT_EQ(data->getDataType(),"string");
-      EXPECT_EQ(data->getMinOccurs(),1);
-      EXPECT_EQ(data->getMaxOccurs(),1);
-      EXPECT_EQ(data->getAllowedValues().size(),3);
+    if ("string_with_options_parameter" == data->getIdentifier()) {
+      EXPECT_EQ(data->getTitle(), "Title of string_with_options_parameter");
+      EXPECT_EQ(data->getDataType(), "string");
+      EXPECT_EQ(data->getMinOccurs(), 1);
+      EXPECT_EQ(data->getMaxOccurs(), 1);
+      EXPECT_EQ(data->getAllowedValues().size(), 3);
       int count{0};
-      for(auto&v:data->getAllowedValues()){
-        if (v=="‘option1’"||v=="‘options2’"||v=="‘option3’" ){
+      for (auto& v : data->getAllowedValues()) {
+        if (v == "‘option1’" || v == "‘option2’" || v == "‘option3’") {
           count++;
         }
       }
-      EXPECT_EQ(count,3);
+      EXPECT_EQ(count, 3);
     }
 
-
-
-
     std::cout << "getDataType: " << data->getDataType() << "\n";
-    std::cout << "getAllowedValuesSize: "
-              << data->getAllowedValues().size() << "\n";
+    std::cout << "getAllowedValuesSize: " << data->getAllowedValues().size()
+              << "\n";
     for (auto& a : data->getAllowedValues()) {
       std::cout << "AllowedValue: " << a << "\n";
     }
 
-    std::cout << "getDefaultValue: " << data->getDefaultValue() <<"\n";
-
+    std::cout << "getDefaultValue: " << data->getDefaultValue() << "\n";
   }
   std::cout << "--------------------------\n";
 }
@@ -104,31 +96,34 @@ TEST(eoepcaows_02, inputs_list_from_catalog) {
   auto util = std::make_unique<Util>();
   std::string path{util->getLibName()};
 
-  std::cout << "start download OWS..."  <<"\n";
+  std::cout << "start download OWS..."
+            << "\n";
   std::string fileBuffer{""};
-  getFromWeb(fileBuffer,"https://catalog.terradue.com/eoepca-services/search?uid=test_entites");
+  getFromWeb(
+      fileBuffer,
+      "https://catalog.terradue.com/eoepca-services/search?uid=test_entites");
 
   std::cout << "CWL: \n";
   std::cout << fileBuffer << "\n\n\n\n";
 
-
-  std::cout <<"lib: "<< path << "\n";
+  std::cout << "lib: " << path << "\n";
 
   auto lib = std::make_unique<EOEPCA::EOEPCAows>(path);
 
   EXPECT_TRUE(lib->IsValid());
 
-  if(!lib->IsValid()){
+  if (!lib->IsValid()) {
     std::cout << "EOEPCAows lib not valid";
     return;
   }
 
   std::unique_ptr<EOEPCA::OWS::OWSContext,
-      std::function<void(EOEPCA::OWS::OWSContext*)>>
-      ptrContext(lib->parseFromMemory(fileBuffer.c_str(),fileBuffer.size()), lib->releaseParameter);
+                  std::function<void(EOEPCA::OWS::OWSContext*)>>
+      ptrContext(lib->parseFromMemory(fileBuffer.c_str(), fileBuffer.size()),
+                 lib->releaseParameter);
 
-
-  std::cout << "parser... end"  <<"\n";
+  std::cout << "parser... end"
+            << "\n";
 
   EXPECT_NE(nullptr, ptrContext.get());
   if (ptrContext) {
@@ -138,7 +133,6 @@ TEST(eoepcaows_02, inputs_list_from_catalog) {
     for (auto& entry : entries) {
       for (auto& off : entry->getOfferings()) {
         for (auto& processDescription : off->getProcessDescription()) {
-
           std::cout << "----------OUTPUTS------------------\n";
           for (auto& input : processDescription->getInputs()) {
             printResults(input);
@@ -158,31 +152,34 @@ TEST(eoepcaows_02, inputs_list_from_catalog_ref) {
   auto util = std::make_unique<Util>();
   std::string path{util->getLibName()};
 
-  std::cout << "start download OWS..."  <<"\n";
+  std::cout << "start download OWS..."
+            << "\n";
   std::string fileBuffer{""};
-  getFromWeb(fileBuffer,"https://catalog.terradue.com/eoepca-services/search?uid=test_entites_ref");
+  getFromWeb(fileBuffer,
+             "https://catalog.terradue.com/eoepca-services/"
+             "search?uid=test_entites_ref");
 
   std::cout << "CWL: \n";
   std::cout << fileBuffer << "\n\n\n\n";
 
-
-  std::cout <<"lib: "<< path << "\n";
+  std::cout << "lib: " << path << "\n";
 
   auto lib = std::make_unique<EOEPCA::EOEPCAows>(path);
 
   EXPECT_TRUE(lib->IsValid());
 
-  if(!lib->IsValid()){
+  if (!lib->IsValid()) {
     std::cout << "EOEPCAows lib not valid";
     return;
   }
 
   std::unique_ptr<EOEPCA::OWS::OWSContext,
-      std::function<void(EOEPCA::OWS::OWSContext*)>>
-      ptrContext(lib->parseFromMemory(fileBuffer.c_str(),fileBuffer.size()), lib->releaseParameter);
+                  std::function<void(EOEPCA::OWS::OWSContext*)>>
+      ptrContext(lib->parseFromMemory(fileBuffer.c_str(), fileBuffer.size()),
+                 lib->releaseParameter);
 
-
-  std::cout << "parser... end"  <<"\n";
+  std::cout << "parser... end"
+            << "\n";
 
   EXPECT_NE(nullptr, ptrContext.get());
   if (ptrContext) {
@@ -192,7 +189,6 @@ TEST(eoepcaows_02, inputs_list_from_catalog_ref) {
     for (auto& entry : entries) {
       for (auto& off : entry->getOfferings()) {
         for (auto& processDescription : off->getProcessDescription()) {
-
           std::cout << "----------OUTPUTS------------------\n";
           for (auto& input : processDescription->getInputs()) {
             printResults(input);
@@ -202,10 +198,122 @@ TEST(eoepcaows_02, inputs_list_from_catalog_ref) {
           for (auto& input : processDescription->getOutputs()) {
             printResults(input);
           }
-
         }
       }
     }
   }
 }
 
+TEST(eoepcaows_02, inputs_enum_optional) {
+  auto util = std::make_unique<Util>();
+  std::string path{util->getLibName()};
+
+  std::cout << "start download OWS..."
+            << "\n";
+  std::string fileBuffer{""};
+  // getFromWeb(fileBuffer,"https://raw.githubusercontent.com/bbrauzzi/blasCool/master/snapping-evo-stack-creation.0.1.0.develop.cwl");
+  getFromWeb(
+      fileBuffer,
+      "https://raw.githubusercontent.com/EOEPCA/proc-comm-lib-ows/feature/"
+      "input_enum_fix/tests/application/assets/test_entites_ref_2.yaml");
+
+  std::cout << "CWL: \n";
+  std::cout << fileBuffer << "\n\n\n\n";
+
+  std::cout << "lib: " << path << "\n";
+
+  auto lib = std::make_unique<EOEPCA::EOEPCAows>(path);
+
+  EXPECT_TRUE(lib->IsValid());
+
+  if (!lib->IsValid()) {
+    std::cout << "EOEPCAows lib not valid";
+    return;
+  }
+
+  std::string applicationFile = fileBuffer.c_str();
+  std::string head =
+      R"(<?xml version="1.0" encoding="utf-8"?><feed xmlns="http://www.w3.org/2005/Atom"><entry><owc:offering xmlns:owc="http://www.opengis.net/owc/1.0" code="http://www.opengis.net/eoc/applicationContext/cwl"><owc:content type="application/cwl"><![CDATA[)";
+  std::string tail = R"(]]></owc:content></owc:offering></entry></feed>)";
+  applicationFile = head;
+  applicationFile.append(fileBuffer);
+  applicationFile.append(tail);
+
+  std::unique_ptr<EOEPCA::OWS::OWSContext,
+                  std::function<void(EOEPCA::OWS::OWSContext*)>>
+      ptrContext(
+          lib->parseFromMemory(applicationFile.c_str(), applicationFile.size()),
+          lib->releaseParameter);
+
+  std::cout << "parser... end"
+            << "\n";
+
+  EXPECT_NE(nullptr, ptrContext.get());
+  if (ptrContext) {
+    auto& entries = ptrContext->getEntries();
+    EXPECT_EQ(1, entries.size());
+
+    for (auto& entry : entries) {
+      for (auto& off : entry->getOfferings()) {
+        for (auto& processDescription : off->getProcessDescription()) {
+          std::cout << "----------INPUTS------------------\n";
+          for (auto& input : processDescription->getInputs()) {
+            // printResults(input);
+
+            std::cout << "--------------------------\n";
+            std::cout << "getIdentifier: " << input->getIdentifier() << "\n";
+            std::cout << "getType: " << input->getType() << "\n";
+            std::cout << "--------------------------\n";
+            std::cout << "getTitle: " << input->getTitle() << "\n";
+            std::cout << "getAbstract: " << input->getAbstract() << "\n";
+
+            std::cout << "getMinOccurs: " << input->getMinOccurs() << "\n";
+            std::cout << "getMaxOccurs: " << input->getMaxOccurs() << "\n";
+
+            if (input->getType() == "LiteralData") {
+              auto data = dynamic_cast<EOEPCA::OWS::LiteralData*>(input.get());
+              std::cout << "getDataType: " << data->getDataType() << "\n";
+              std::cout << "getAllowedValuesSize: "
+                        << data->getAllowedValues().size() << "\n";
+              for (auto& a : data->getAllowedValues()) {
+                std::cout << "AllowedValue: " << a << "\n";
+              }
+
+              std::cout << "getDefaultValue: " << data->getDefaultValue()
+                        << "\n";
+            }
+          }
+
+          std::cout << "----------OUTPUTS------------------\n";
+          for (auto& input : processDescription->getOutputs()) {
+            // printResults(input);
+
+            std::cout << "--------------------------\n";
+            std::cout << "getIdentifier: " << input->getIdentifier() << "\n";
+            std::cout << "getType: " << input->getType() << "\n";
+            std::cout << "--------------------------\n";
+            std::cout << "getTitle: " << input->getTitle() << "\n";
+            std::cout << "getAbstract: " << input->getAbstract() << "\n";
+
+            std::cout << "getMinOccurs: " << input->getMinOccurs() << "\n";
+            std::cout << "getMaxOccurs: " << input->getMaxOccurs() << "\n";
+            if (input->getType() == "LiteralData") {
+              auto data = dynamic_cast<EOEPCA::OWS::LiteralData*>(input.get());
+              std::cout << "getDataType: " << data->getDataType() << "\n";
+              std::cout << "getAllowedValuesSize: "
+                        << data->getAllowedValues().size() << "\n";
+              for (auto& a : data->getAllowedValues()) {
+                std::cout << "AllowedValue: " << a << "\n";
+              }
+
+              std::cout << "getDefaultValue: " << data->getDefaultValue()
+                        << "\n";
+            }
+          }
+        }
+      }
+    }
+  }
+}
+
+// stack-creation
